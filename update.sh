13a8c468afc672d3cd86a061361a111655c0d071
katze <katzenmaul@mail.ru>
2019-04-25 21:30:16 +0300

    node server cron

--- update.sh
+++ update.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 cd /home/pi/Desktop/tpt/TPTBlockchain
 git up
+npm install
 ng build --prod --base-href /TPTBlockchain/
 rm -rf /var/www/html/TPTBlockchain/
 cp -r ./dist/TPTBlockchain /var/www/html/
 cp .htaccess /var/www/html/TPTBlockchain
+cd NodeServer
+forever stop /home/pi/Desktop/tpt/TPTBlockchain/NodeServer/index.js
+npm install
+forever start /home/pi/Desktop/tpt/TPTBlockchain/NodeServer/index.js

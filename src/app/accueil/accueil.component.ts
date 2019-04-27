4374ef6f658c45cd39b7120573b6e6728a902044
SergioSim <sergio.simonian@etu.unice.fr>
2019-04-27 20:54:57 +0200

    adding background to main page

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+    this.renderer.setStyle(document.body, 'height', '100vh');
+   }
 
   ngOnInit() {
   }

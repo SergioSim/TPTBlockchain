2cf7fe0bf59ac971740d6ab6c9f2268ca228e419
SergioSim <sergio.simonian@etu.unice.fr>
2019-04-28 15:58:24 +0200

    fix routing issue

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -13,9 +13,13 @@ export class AccueilComponent implements OnInit {
 
   ngOnInit() {
     document.querySelector("#Group_334").addEventListener("click", () => location.href = "banqueCentrale");
+    document.querySelector("#Banque_Centrale").addEventListener("click", () => location.href = "banqueCentrale");
     document.querySelector("#Group_334-2").addEventListener("click", () => location.href = "banqueCommerciale");
+    document.querySelector("#Banque_Commerciale").addEventListener("click", () => location.href = "banqueCommerciale");
     document.querySelector("#Group_334-3").addEventListener("click", () => location.href = "commercant");
+    document.querySelector("#Commercant").addEventListener("click", () => location.href = "commercant");
     document.querySelector("#Group_334-4").addEventListener("click", () => location.href = "particulier");
+    document.querySelector("#Particulier").addEventListener("click", () => location.href = "particulier");
   }
 
 }

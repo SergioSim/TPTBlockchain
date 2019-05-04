7e0698b52bd8ec6d7ce182049718784d46f95bf9
SergioSim <sergio.simonian@etu.unice.fr>
2019-05-04 11:59:30 +0200

    fix minor image issue

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -14,16 +14,16 @@ export class AccueilComponent implements OnInit {
 
   constructor(private renderer: Renderer2) {
     this.renderer.setStyle(document.body, 'height', '100%');
-    this.brh = {name: 'brh', image: '../../../assets/logos/brh_gradient_logo.jpeg', url: '/brh'};
+    this.brh = {name: 'brh', image: 'assets/logos/brh_gradient_logo.jpeg', url: '/brh'};
     this.banks = [
-      {name: 'sogebank', image: '../../../assets/logos/sogebank_logo.png', url: '/sogebank'},
-      {name: 'scotiabank', image: '../../../assets/logos/scotiabank_logo.png', url: 'scotiabank.com'},
-      {name: 'capital bank', image: '../../../assets/logos/capital_bank_logo.jpg', url: 'capital-bank.com'},
-      {name: 'buh', image: '../../../assets/logos/buh_logo.png', url: 'buh.com'},
-      {name: 'citibank', image: '../../../assets/logos/citibank_logo.png', url: 'citibank.com'},
-      {name: 'unibank', image: '../../../assets/logos/unibank_logo.png', url: 'unibank.com'},
-      {name: 'bph', image: '../../../assets/logos/bph_logo.jpeg', url: 'bph.com'},
-      {name: 'bnc', image: '../../../assets/logos/bnc_logo.png', url: 'bnc.com'}
+      {name: 'sogebank', image: 'assets/logos/sogebank_logo.png', url: '/sogebank'},
+      {name: 'scotiabank', image: 'assets/logos/scotiabank_logo.png', url: 'scotiabank.com'},
+      {name: 'capital bank', image: 'assets/logos/capital_bank_logo.jpg', url: 'capital-bank.com'},
+      {name: 'buh', image: 'assets/logos/buh_logo.png', url: 'buh.com'},
+      {name: 'citibank', image: 'assets/logos/citibank_logo.png', url: 'citibank.com'},
+      {name: 'unibank', image: 'assets/logos/unibank_logo.png', url: 'unibank.com'},
+      {name: 'bph', image: 'assets/logos/bph_logo.jpeg', url: 'bph.com'},
+      {name: 'bnc', image: 'assets/logos/bnc_logo.png', url: 'bnc.com'}
     ]
    }
 

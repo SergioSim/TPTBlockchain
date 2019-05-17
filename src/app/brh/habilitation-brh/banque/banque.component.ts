25d046431c54a8c4ac0302e79f88620fe4fde973
SergioSim <sergio.simonian@etu.unice.fr>
2019-05-17 20:34:18 +0200

    delete second banque-list.component

--- src/app/brh/habilitation-brh/banque/banque.component.ts
+++ src/app/brh/habilitation-brh/banque/banque.component.ts
@@ -8,7 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class BanqueComponent implements OnInit {
 
-  constructor(private service: NodeapiService) 
+  constructor(public service: NodeapiService) 
      { }
 
      ngOnInit() {

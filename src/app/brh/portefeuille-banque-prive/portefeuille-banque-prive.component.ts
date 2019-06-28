f9e946a1903d06aa50c27c5660bd225f1b8f2caa
SergioSim <sergio.simonian@etu.unice.fr>
2019-06-29 01:25:42 +0200

    copy paste SogeBank wallet edit to bank admin page

--- src/app/brh/portefeuille-banque-prive/portefeuille-banque-prive.component.ts
+++ src/app/brh/portefeuille-banque-prive/portefeuille-banque-prive.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
-import { NodeapiService } from 'src/app/nodeapi.service';
+import { NodeapiService, apiUrl } from 'src/app/nodeapi.service';
 import { CommonUtilsService } from 'src/app/common/common-utils.service';
 import { Portefeuille } from '../clients-banque-prive/clients-banque-prive.component';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-portefeuille-banque-prive',
@@ -12,10 +13,16 @@ import { Portefeuille } from '../clients-banque-prive/clients-banque-prive.compo
 export class PortefeuilleBanquePriveComponent implements OnInit {
 
   public selectedPortefeuille: Portefeuille;
+  private copyLibelle: string;
   faPen = faPen;
   public selectedSolde = 0;
+  editDialogRef: any;
 
-  constructor(public apiService: NodeapiService, public commonUtilsService: CommonUtilsService) { }
+  constructor(
+    public apiService: NodeapiService,
+    public commonUtilsService: CommonUtilsService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     this.selectedPortefeuille = this.apiService.portefeuilles[0];
@@ -24,8 +31,34 @@ export class PortefeuilleBanquePriveComponent implements OnInit {
     });
   }
 
-  openEditWalletDialog() {
-    console.log('TODO implementation');
+  openEditWalletDialog(atemplate) {
+    this.copyLibelle = this.selectedPortefeuille.Libelle;
+    this.editDialogRef = this.dialog.open(atemplate, { width: '400px' });
+  }
+
+  confirmWalletEdit() {
+    const editPortefeuilleDetails = {
+      libelle: this.selectedPortefeuille.Libelle,
+      id: this.selectedPortefeuille.Id
+    };
+    this.apiService.makeRequest(apiUrl.updatePortefeuilleLibelle, editPortefeuilleDetails).subscribe(
+      res => {
+        this.editDialogRef.close();
+        this.snackBar.open(this.selectedPortefeuille.Libelle + ' à bien été mis à jour.', 'Fermer', {
+          duration: 5000,
+        });
+        this.apiService.portefeuilles[0].Libelle = this.selectedPortefeuille.Libelle;
+      }, error => {
+        this.snackBar.open('Le portefeuille n\'a pas pu être mise à jour, veuillez réessayer.', 'Fermer', {
+          duration: 5000,
+        });
+        this.selectedPortefeuille.Libelle = this.copyLibelle;
+      });
+  }
+
+  cancelWalletEdit() {
+    this.selectedPortefeuille.Libelle = this.copyLibelle;
+    this.editDialogRef.close();
   }
 
 }

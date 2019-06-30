ca9486f38bb3f7bc205b40f2e83f62d7b329fe4b
SergioSim <sergio.simonian@etu.unice.fr>
2019-07-01 00:28:57 +0200

    fix bank habilitation issue

--- src/app/brh/demande-habilitation/demande-habilitation.component.ts
+++ src/app/brh/demande-habilitation/demande-habilitation.component.ts
@@ -3,6 +3,7 @@ import { NodeapiService, apiUrl } from 'src/app/nodeapi.service';
 import { MatSnackBar } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
 import { InputErrorStateMatcher } from './InputErrorStatMatcher';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -23,17 +24,18 @@ export class DemandeHabilitationComponent implements OnInit {
   nouvelBanquePassword: FormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
   nouvelBanquePasswordConfirm: FormControl = new FormControl('', [Validators.required, Validators.minLength(6)]);
   nouvelBanqueTel: FormControl = new FormControl('', [Validators.required, Validators.minLength(9)]);
- 
+
 
   matcher = new InputErrorStateMatcher();
   constructor(
     private service: NodeapiService,
-    private snackBar: MatSnackBar) { }
+    private snackBar: MatSnackBar,
+    public router: Router) { }
 
   ngOnInit() {
   }
 
-  confirmAddBanque(){
+  confirmAddBanque() {
     if (this.BanqueNom.invalid || this.nouvelBanquePrenom.invalid || this.nouvelBanqueNom.invalid || this.nouvelBanqueTel.invalid
       || this.nouvelBanquePassword.invalid || this.nouvelBanquePasswordConfirm.invalid) {
     this.snackBar.open('Tous les champs sont requis!', 'Fermer', {
@@ -42,13 +44,12 @@ export class DemandeHabilitationComponent implements OnInit {
     });
     return;
   }
-  console.log(this.nouvelBanquePasswordConfirm);
-  console.log('-----------------------');
+    console.log(this.nouvelBanquePasswordConfirm);
+    console.log('-----------------------');
 
-  console.log(this.nouvelBanquePassword);
+    console.log(this.nouvelBanquePassword);
 
-  
-  if (this.nouvelBanquePasswordConfirm.value !== this.nouvelBanquePassword.value) {
+    if (this.nouvelBanquePasswordConfirm.value !== this.nouvelBanquePassword.value) {
     this.snackBar.open('La confirmation du mot de passe doit être identique au mot de passe', 'Fermer', {
       duration: 5000,
       panelClass: ['alert-snackbar']
@@ -56,23 +57,27 @@ export class DemandeHabilitationComponent implements OnInit {
     return;
   }
 
-    this.service.makeRequest(apiUrl.createBank, {name:this.BanqueNom.value,email:this.nouvelBanqueEmail.value,telephone:this.nouvelBanqueTel.value,isVisible:1,statut:'en cours'}).
-    subscribe( res =>{
+    this.service.makeRequest(apiUrl.createBank,
+      {name: this.BanqueNom.value, email: this.nouvelBanqueEmail.value,
+        telephone: this.nouvelBanqueTel.value, isVisible: 1, statut: 'en cours'}).
+    subscribe( res => {
       console.log('on a recu la response:' );
       console.log(res);
 
       this.service.makeRequest(apiUrl.createClient, {
         email: this.nouvelBanqueEmail.value, password: this.nouvelBanquePassword.value,
-        prenom: this.nouvelBanquePrenom.value, nom: this.nouvelBanqueNom.value, banque: this.nouvelBanqueNom.value, roleId: 1
+        prenom: this.nouvelBanquePrenom.value, nom: this.nouvelBanqueNom.value, banque: this.BanqueNom.value, roleId: 1
       }).
-        subscribe(res => {
-          console.log('on a recu la response:');
-          console.log(res);
+        subscribe( res2 => {
+          if (res2 && res2.success === true) {
+            this.snackBar.open('Votre demande d\'habilitation a été enregistrée.',
+            'Fermer', { duration: 5000, panelClass: ['succes-snackbar']});
+            this.router.navigateByUrl('/brh/banque/login');
+          }
         }, error => {
           console.log('got an error 1');
           console.log(error);
         });
-      this.snackBar.open('Votre demande d\'habilitation a été enregistrée.', 'Fermer', { duration: 5000, });
 
     }, error => {
         console.log('got an error 2');
@@ -81,6 +86,10 @@ export class DemandeHabilitationComponent implements OnInit {
 
         console.log(error);
     });
+  }
+
+  goBack() {
+    this.router.navigateByUrl('/brh/accueil');
   }
 
   openAnnonceLegaleInput() {

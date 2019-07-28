7c208282c20f23f3e2c937e503d8456a73939f10
SergioSim <Sergio.Simonian@etu.unice.fr>
2019-07-28 17:52:31 +0200

    update autoupdate script

--- update.sh
+++ update.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
+now=$(date +"%T")
+START=$(date +%s)
+echo $now START UPDATE
 cd /home/tptblockchain/Desktop/TPTBlockchain
 git pull
 npm install
-ng build --prod --base-href /TPTBlockchain/
+/home/tptblockchain/Desktop/TPTBlockchain/node_modules/@angular/cli/bin/ng build --prod --base-href /TPTBlockchain/
 rm -rf /var/www/html/TPTBlockchain/*
 cp -r ./dist/TPTBlockchain /var/www/html/
 cp .htaccess /var/www/html/TPTBlockchain
 cd NodeServer
-forever stop index.js
+/usr/local/lib/node_modules/forever/bin/forever stop index.js
 npm install
-forever start index.js
+/usr/local/lib/node_modules/forever/bin/forever start index.js
+now=$(date +"%T")
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo $now END UPDATE
+echo "The UPDATE TOOK $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds!"
